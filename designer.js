3ee3cefa28bc1999676a11111e7236fb7a042351
Dan Gilmore <dan@dangilmore.com>
2021-02-03 17:58:33 -0500

    Added more CSS for the form, and created designer.js

---
+++ designer.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function() {
+    //
+    resultLength.innerHTML = document.getElementById('inputLength').value + "in x ";
+    resultWidth.innerHTML = document.getElementById('inputWidth').value + "in";
+});
+
+
+sourceLength = document.getElementById('inputLength');
+sourceWidth = document.getElementById('inputWidth');
+
+resultWidth = document.getElementById('resultWidth');
+
+WidthInputHandler = function(e) {
+    resultWidth.innerHTML = e.target.value +" in";
+}
+
+LengthInputHandler = function(e) {
+    resultLength.innerHTML = e.target.value +" in x ";
+}
+
+sourceWidth.addEventListener('input', WidthInputHandler);
+sourceWidth.addEventListener('propertychange', WidthInputHandler);
+
+sourceLength.addEventListener('input', LengthInputHandler);
+sourceLength.addEventListener('propertychange', LengthInputHandler);
--- designer.php
+++ designer.php
@@ -7,7 +7,7 @@
 get_header();
 
 wp_enqueue_style( 'designer_css', 'http://www.dancywood.dev.cc/wp-content/themes/cuttingboarddesigner/designer.css'  );
-
+wp_enqueue_script( 'designer_js', 'http://www.dancywood.dev.cc/wp-content/themes/cuttingboarddesigner/designer.js');
 ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
@@ -18,42 +18,72 @@ wp_enqueue_style( 'designer_css', 'http://www.dancywood.dev.cc/wp-content/themes
                 </h1>
             </header>
             <div class="entry-content">
-                <div class="designer-container">
-                    <div class="designer-header">
-                        <p>
-                            Choose the size, types of wood, and layout that you would like. We will create a custom 
-                            board using your design, and overall size. Please note that due to variations in
-                            wood, we cannot guarantee that the strips will be the exact size you requested, 
-                            but we will get it as close as possible.
-                        </p>
-                        <p>Size limitations
-                            <ul>
-                                <li>Length: 16"-22"</li>
-                                <li>Width: 8"-12.5"</li>
-                                <li>Thickness: 3/4"-1.5"</li>
-                            </ul>
-                        </p>
-                    </div><!-- end class="designer-header" -->
-                    <div class="designer-form">
-                        designer-form
-                        <div class="designer-parameters">
-                            designer-parameters
-                            <div class="designer-measurements">
-                                designer-measurements
-                            </div>
-                            <div class="designer-layout">
-                                designer-layout 
-                            </div>
-                            <div class="designer-layout-instructions">
-                                designer-layout-instructions 
-                            </div>
-                        </div><!-- end of class="designer-parameters" -->
+                <div class="designer-header">
+                    <p>
+                        Choose the size, types of wood, and layout that you would like. We will create a custom 
+                        board using your design, and overall size. Please note that due to variations in
+                        wood, we cannot guarantee that the strips will be the exact size you requested, 
+                        but we will get it as close as possible.
+                    </p>
+                    <p>Size limitations
+                        <ul>
+                            <li>Length: 16"-22"</li>
+                            <li>Width: 8"-12.5"</li>
+                            <li>Thickness: 3/4"-1.5"</li>
+                        </ul>
+                    </p>
+                </div><!-- end class="designer-header" -->
+                <div class="designer-form-container">
 
+                    <div class="designer-form-left">
+                        <div class="designer-measurements">
+                            <label for="length" >Length</label>
+                            <input type="number" step="0.25" min="16" max="22" value="16" id="inputLength" name="length" class="input-control" id="inputLength">
+                            <label for="width">Width</label>
+                            <input type="number" step="0.25" min="8" max="12.5" value="12" id="inputWidth" name="width" class="input-control" >
+                            <label for="thickness">Thickness</label>
+                            <input type="number" step="0.25" min="0.75" max="1.5" value="1" id="thickness" name="thickness" class="input-control" id="thickness">  
+                        </div>
+                        <div class="designer-pattern">
+                            <label for="pattern">Pattern</label>
+                            <input type="text" id="layout" name="pattern" value="1W 1M 1W 1M 1W 1M 1W 1M 1W 1M 1W 1M">
+                        </div>
+                        <div class="designer-layout-instructions">
+                            <h3>Pattern Format</h3>
+
+Patterns consist of one or more blocks that have a wood type and a size. For example, "1W 1M" would represent 1in of walnut followed by 1in of maple.
+
+valid wood types are (C) Cherry (M) Maple (P) Padauk (PH) Purple Heart (W) Walnut 
+                        </div>
+                    </div>
+                    <div class="designer-form-right">
                         <div class="designer-preview">
-                            designer-preview
+                            <div class="designer-preview-title">
+                                Preview
+                            </div>
+                            <div class="designer-preview-dimensions">
+                                <span id="resultLength"> in x </span>
+                                <span id="resultWidth"> in</span>
+                            </div>
                         </div><!-- end of class="designer-preview" -->
-                    </div><!-- end of class="designer-form" -->
-                </div><!-- end of class="designer-container" -->
+                    </div>
+                </div>
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
             </div><!-- end of class="entry-content" -->
         </article>  
     </main><!-- .site-main -->
